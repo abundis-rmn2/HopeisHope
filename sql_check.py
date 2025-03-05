c32c92f89ff2e2f338a9bc1fe4fb7865c7ca97e9
abundis-rmn2 <abundis@rmn2.com>
2025-03-04 18:03:45 -0600

    graphs_sql.py added

--- sql_check.py
+++ sql_check.py
@@ -2,17 +2,25 @@ import json
 import mysql.connector
 from mysql.connector import Error
 
-
 # Load MySQL credentials from external file
-def load_db_credentials():
-    with open('db_credentials.json', 'r') as file:
-        credentials = json.load(file)
-    return credentials
-
+def load_db_credentials(file_path='db_credentials.json'):
+    try:
+        with open(file_path, 'r') as file:
+            credentials = json.load(file)
+        return credentials
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+        return None
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON from the credentials file.")
+        return None
 
 # Check MySQL connection
 def check_mysql_connection():
     credentials = load_db_credentials()
+
+    if credentials is None:
+        return
 
     try:
         # Establish connection to MySQL database
@@ -25,21 +33,18 @@ def check_mysql_connection():
 
         if connection.is_connected():
             print("Connection to MySQL database successful")
-            return connection
-        else:
-            print("Failed to connect to MySQL database")
-
     except Error as e:
         print(f"Error: {e}")
-        return None
-
+    finally:
+        if connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
 
 # Close the connection
 def close_connection(connection):
     if connection:
         connection.close()
         print("MySQL connection closed")
-
 
 # Example usage
 if __name__ == "__main__":
